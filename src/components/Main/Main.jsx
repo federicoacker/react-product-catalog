d6ff07c95321898d073d49566590dabfeeff12b7
federicoacker <acker.federico@gmail.com>
2026-05-11 14:45:08 +0200

    Aggiunta variabile di stato products al Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,24 +2,35 @@ import FilterList from "./FilterList.jsx";
 import ProductList from "./ProductList.jsx";
 import fetchProducts from "../../utils/fetchProducts.js";
 import { useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
 
 function Main() {
+    const [products, setProducts] = useState([])
+    console.log(products);
 
     useEffect(
         () => {
             fetchProducts()
-            .then(
-                products => console.log(products)
-            )
-            .catch(error => console.error(error));
-        },[]
+                .then(
+                    products => setProducts(products)
+                )
+                .catch(error => console.error(error));
+        }, []
     );
 
     return (
         <main>
-            <div>Main</div>
-            <FilterList></FilterList>
-            <ProductList></ProductList>
+            <Container>
+                <Row>
+                    <Col xs={4}>
+                        <FilterList></FilterList>
+                    </Col>
+                    <Col xs={8}>
+                        <ProductList></ProductList>
+                    </Col>
+                </Row>
+            </Container>
         </main>
     )
 }
